4b473f73bf7f23955224578567af8ae876fed7d7
reconelement <omkarpanda895@gmail.com>
2025-10-26 23:52:25 +0530

    refactored the code and arranged the modules

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,42 @@
 
-mod db {
+pub mod db {
+    use sqlx::{Executor, Pool, Postgres};
+    use sqlx::Error;
+    use sqlx::postgres::{PgQueryResult};
+    pub async fn seed_data(connection: Pool<Postgres>) -> Result<PgQueryResult, Error> {
+        let query: &str = "
+        CREATE TABLE \"User\" (
+            \"id\" SERIAL NOT NULL,
+            \"email\" TEXT NOT NULL,
+            \"name\" TEXT,
+            \"password\" TEXT,
+            \"isDeleted\" BOOLEAN,
+            CONSTRAINT \"User_pkey\" PRIMARY KEY (\"id\")
+        );
+    ";
 
+        let query2: &str = "
+        CREATE TABLE \"Post\" (
+            \"id\" SERIAL NOT NULL,
+            \"createdAt\" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            \"updatedAt\" TIMESTAMP(3)
+            \"title\" TEXT NOT NULL,
+            \"authorId\" INTEGER NOT NULL,
+            CONSTRAINT \"Post_pkey\" PRIMARY KEY (\"id\")
+        );
+    ";
+
+        let query3: &str = "
+        CREATE UNIQUE INDEX \"User_email_key\" ON \"User\"(\"email\");
+    ";
+
+        let query4: &str = "
+        ALTER TABLE \"Post\" ADD CONSTRAINT \"Post_authorId_fkey\" FOREIGN KEY (\"authorId\") REFERENCES \"User\"(\"id\") ON DELETE RESTRICT ON UPDATE CASCADE;
+    ";
+        let parts = [query, query2, query3, query4];
+        let coll = parts.concat();
+        let string_slice: &str = coll.as_str();
+        let result = connection.execute(string_slice).await;
+        result
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 use axum::{
     routing::get,
     Router,
@@ -12,6 +13,7 @@ use sqlx::postgres::{
     
 };
 use sqlx::{Executor, Pool, Postgres};
+mod db;
 #[derive(Serialize, Deserialize, Debug)]
 struct User{
     full_name: String,
@@ -32,48 +34,11 @@ async fn main(){
 async fn signup(Json(payload): Json<serde_json::Value>){
     println!("{:?}",payload);
     let connection = connect_to_db().await;
-    let query = seed_data(connection).await;
+    let query = db::db::seed_data(connection).await;
 }
 //postgres password 2020
 async fn connect_to_db()->Pool<Postgres>{
     let pool = PgPoolOptions::new().max_connections(5).connect("postgres://postgres:2002@localhost/postgres").await.expect("successfully connected to db");
     pool
-}
-
-async fn seed_data(connection: Pool<Postgres>)->Result<PgQueryResult, Error>{
-    let query: &str = "
-        CREATE TABLE \"User\" (
-            \"id\" SERIAL NOT NULL,
-            \"email\" TEXT NOT NULL,
-            \"name\" TEXT,
-            \"password\" TEXT,
-            \"isDeleted\" BOOLEAN,
-            CONSTRAINT \"User_pkey\" PRIMARY KEY (\"id\")
-        );
-    ";
-     
-    let query2: &str = "
-        CREATE TABLE \"Post\" (
-            \"id\" SERIAL NOT NULL,
-            \"createdAt\" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            \"updatedAt\" TIMESTAMP(3)
-            \"title\" TEXT NOT NULL,
-            \"authorId\" INTEGER NOT NULL,
-            CONSTRAINT \"Post_pkey\" PRIMARY KEY (\"id\")
-        );
-    ";
-
-    let query3: &str = "
-        CREATE UNIQUE INDEX \"User_email_key\" ON \"User\"(\"email\");
-    ";
-
-    let query4: &str = "
-        ALTER TABLE \"Post\" ADD CONSTRAINT \"Post_authorId_fkey\" FOREIGN KEY (\"authorId\") REFERENCES \"User\"(\"id\") ON DELETE RESTRICT ON UPDATE CASCADE;
-    ";
-    let parts = [query, query2, query3, query4];
-    let coll = parts.concat();
-    let string_slice: &str = coll.as_str();
-    let result = connection.execute(string_slice).await;
-    result
 }
 
