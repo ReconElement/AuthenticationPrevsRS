4da46d553385365ba0729afb8fed7c7a95fe92db
reconelement <omkarpanda895@gmail.com>
2025-11-03 18:18:41 +0530

    Removed sea-orm, use only sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
+use bcrypt::{DEFAULT_COST, hash, verify};
 use dotenv::dotenv;
-mod entity;
-use entity::{post, user};
 use axum::{
     routing::get,
     Router,
@@ -21,6 +20,7 @@ use sqlx::postgres::{
 };
 use sqlx::{Executor, Pool, Postgres};
 mod db;
+use db::db::{seed_data, sign_up_query, sign_in_query};
 #[derive(Serialize, Deserialize, Debug)]
 struct User{
     full_name: String,
@@ -28,18 +28,35 @@ struct User{
     user_name: String,
     password: String
 }
+#[derive(Serialize, Deserialize, Debug)]
+struct sign_in_user{
+    full_name: String,
+    password: String
+}
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/",get(|| async {"Hello World"})).route("/signup",post(signup));
+    let app = Router::new().route("/",get(|| async {"Hello World"})).route("/signup",post(signup)).route("/signin",post(signin));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn signup(Json(user): Json<User>)->StatusCode{
     println!("{:?}",user);
-    //write to db 
-    let db_conn = connect_to_db_sea().await;
+    //write to db
+    let db_conn = connect_to_db().await;
+    // let _ = seed_data(db_conn).await;
+    let password = user.password.clone();
+    let hashed_password = hash(user.password, DEFAULT_COST).unwrap();
+    let valid = verify(password, &hashed_password).unwrap();
+    println!("{}",valid);
+    let insert = sign_up_query(db_conn, &user.email,&user.full_name, &hashed_password, false).await;
+    StatusCode::ACCEPTED
+}
+async fn signin(Json(sign_in_user):Json<sign_in_user>)->StatusCode{
+    let connection = connect_to_db().await;
+    let signin = sign_in_query(connection, &sign_in_user.full_name, &sign_in_user.password).await;
+    
     StatusCode::ACCEPTED
 }
 //postgres password 2020
@@ -50,6 +67,6 @@ async fn connect_to_db_sea()->DatabaseConnection{
     db
 }
 async fn connect_to_db()->Pool<Postgres>{
-    let pool = PgPoolOptions::new().max_connections(5).connect("postgres://postgres:2020@localhost/postgres").await.expect("successfully connected to db");
+    let pool = PgPoolOptions::new().max_connections(5).connect("postgres://postgres:2020@localhost/postgres").await.expect("Connection with the db could not be established");
     pool
 }
